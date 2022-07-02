cabc9bb2e322cf637b743efee58c3cfb206d5e3b
MaximBabenkov <lostbass@mail.ru>
2022-07-02 16:56:59 +0300

    Methods for initial array

---
+++ Task/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк,
+длина которых меньше либо равна 3 символа */
+
+string[]CreateArrStr(int count)
+{
+    return new string[count];
+}
+
+void FillArrStr(string[]str)
+{
+    for (int i=0;i<str.Length;i++)
+    {
+        str[i]=Console.ReadLine()??"0";
+    }   
+}
+
+void PrintArrStr(string[]arr)
+{
+    for (int i=0;i<arr.Length;i++)
+    Console.Write($"{arr[i]} ");
+}
+
